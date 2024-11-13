c549763af3959354eba8f673327bcc402b801cc2
saatvik1213 <Saatvik1213@gmail.com>
2024-11-12 17:30:54 -0700

    Uopdated basic

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	title: `SocialScape | A Capital Raising service with no hassle`,
+	description: `SocialScape is a capital raising service that helps you raise capital for your business without the hassle of traditional methods. We help you raise capital through social media and other online platforms.`,
+	url: `https://trysocialscape.co.uk`, // No trailing slash!
+	name: `SocialScape`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
