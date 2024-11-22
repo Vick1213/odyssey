83aa4b738a05e31b5834d9668e2e09a0f4971aa8
saatvik1213 <165426534+Vick1213@users.noreply.github.com>
2024-11-21 17:29:59 -0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,9 @@ export default defineConfig({
   site: 'https://trysocialscape.co.uk', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+
+  output: 'server',
+  adapter: vercel({
+           imageService: true
+           })
 });
